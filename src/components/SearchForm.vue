782d46878669d9d73dece0beb553f2407447d2dd
mkuzie <mkuzie@users.noreply.github.com>
2021-02-27 02:05:15 -0600

    Implemented the SearchForm component and updated main.js to use project dependencies.

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    Search Bar
+    <b-form inline @submit="onSubmit">
+      <b-input-group>
+        <!-- Input Field -->
+        <b-form-input v-model="form.tag" placeholder="Enter a tag to search by" required />
+
+        <!-- Search Button -->
+        <b-input-group-append>
+          <b-button type="submit" variant="primary">
+            <b-icon-search/>
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SearchForm"
+  name: "SearchForm",
+  data() {
+    return {
+      form: {
+        tag: null
+      }
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      this.$emit("search", this.form)
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import dayjs from 'dayjs'
+import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.prototype.$dayjs = dayjs
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
